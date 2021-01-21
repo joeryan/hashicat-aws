53c586bc6b0a9f2f97a475a7350d9c88728cd2e3
root <joe@clanryan.us>
2021-01-21 20:21:56 +0000

    udpated bucket_prefix in tfvars

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket_example_complete" {
   source  = "app.terraform.io/joer-training/s3-bucket/aws//examples/complete"
   version = "1.15.0"
-  bucket_prefix = "joeryan-gaurav-initial"
+  
 }
